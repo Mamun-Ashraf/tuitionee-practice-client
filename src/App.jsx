0ead32bf21775692ebbefa48bb427027029a2e65
Mamun-Ashrsf <ashrafdkia@gmail.com>
2023-11-21 19:52:05 +0600

    setup basic project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h1 className='text-4xl font-bold'>Find your best <span className='text-yellow-500'>House Tutor</span></h1>
+    </div>
+  )
+}
+
+export default App
