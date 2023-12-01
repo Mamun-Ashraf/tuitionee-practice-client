53be3248c90fff73b9befec36c07fe5729056640
Mamun-Ashrsf <ashrafdkia@gmail.com>
2023-12-01 16:55:42 +0600

    bangla translations for leaflets added

--- src/Pages/Leaflet/AdmissionLeaflet.jsx
+++ src/Pages/Leaflet/AdmissionLeaflet.jsx
@@ -6,7 +6,9 @@ import LoginButton from '../../Components/GenericButton/LoginButton';
 
 const AdmissionLeaflet = () => {
 
-    const { darkMode } = useHeader();
+    const { darkMode, translations } = useHeader();
+    const { heading, address, division, district, upazila, area, description, negotiable, term, termLink, postLeaflet } = translations.leafletHouseTuition;
+    const { institutesIntro, instituteName, institutesFeature, describe } = translations.admissionLeaflet;
     const { register, handleSubmit, formState: { errors } } = useForm();
 
     const handleLeaflet = data => {
@@ -15,47 +17,47 @@ const AdmissionLeaflet = () => {
 
     return (
         <div className={`w-5/6 mx-auto p-2 sm:p-7 rounded ${darkMode ? 'dark' : 'bg-grayBody text-blue-Foot'}`}>
-            <h2 className='text-xl sm:text-2xl font-bold text-center text-blueFoot dark:text-grayBody mb-10'>This information will assist you in reaching more people and receiving faster responses.</h2>
+            <h2 className='text-xl sm:text-2xl font-bold text-center text-blueFoot dark:text-grayBody mb-10'>{heading}</h2>
             <form onSubmit={handleSubmit(handleLeaflet)}>
                 <div className='md:flex gap-6'>
                     <div className='sm:w-11/12 md:w-1/2 mx-auto bg-base-100 shadow-md shadow-darkColor dark:shadow-grayBody p-5 rounded space-y-2 mb-6 md:mb-0'>
-                        <h2 className='text-center font-semibold text-lg underline'>Institutes Intro</h2>
+                        <h2 className='text-center font-semibold text-lg underline'>{institutesIntro}</h2>
                         <div>
-                            <label htmlFor="" className='font-bold'>Institutes Name :</label>
+                            <label htmlFor="" className='font-bold'>{instituteName} :</label>
                             <input type="number" name="" id="" className='p-2 block sm:ms-16 focus:outline-none'></input>
                         </div>
                         <div>
-                            <label htmlFor="" className='font-bold'>Address :</label>
+                            <label htmlFor="" className='font-bold'>{address} :</label>
                             <div className='grid grid-cols-1 md:grid-cols-2 gap-5 ms-16'>
                                 <select name="" id="" className='p-2 focus:outline-none'>
-                                    <option>Division</option>
+                                    <option>{division}</option>
                                 </select>
                                 <select name="" id="" className='p-2 focus:outline-none'>
-                                    <option>District</option>
+                                    <option>{district}</option>
                                 </select>
                                 <select name="" id="" className='p-2 focus:outline-none'>
-                                    <option>Upazila</option>
+                                    <option>{upazila}</option>
                                 </select>
-                                <input type="text" placeholder='Your current location' className='p-2 focus:outline-none'></input>
+                                <input type="text" placeholder={area} className='p-2 focus:outline-none'></input>
                             </div>
                         </div>
                     </div>
 
                     <div className='sm:w-11/12 md:w-1/2 mx-auto bg-base-100 shadow-md shadow-darkColor dark:shadow-grayBody p-5 rounded space-y-2'>
-                        <h2 className='text-center font-semibold text-lg underline'>Institutes Characteristics</h2>
+                        <h2 className='text-center font-semibold text-lg underline'>{institutesFeature}</h2>
 
                         <div>
-                            <label htmlFor="" className='font-bold'>Description :</label>
-                            <textarea name="" id="" cols="30" rows="10" className='p-2 block sm:ms-16 focus:outline-none' placeholder="Please mention here your circular"></textarea>
+                            <label htmlFor="" className='font-bold'>{description} :</label>
+                            <textarea name="" id="" cols="30" rows="10" className='p-2 block sm:ms-16 focus:outline-none' placeholder={describe}></textarea>
                         </div>
                     </div>
                 </div>
                 <div className="flex items-center my-5">
                     <input id="default-checkbox" type="checkbox" value="" className="w-4 h-4 text-blueFoot bg-gray-100 border-gray-300 rounded" />
-                    <label htmlFor="default-checkbox" className="ml-2 text-sm font-medium">I have read and accept the <Link to='/terms' className='text-yellowBtn'>terms and conditions</Link></label>
+                    <label htmlFor="default-checkbox" className="ml-2 text-sm font-medium">{term} <Link to='/terms' className='text-yellowBtn'>{termLink}</Link></label>
                 </div>
                 <div className='w-52 mx-auto'>
-                    <LoginButton>Post Leaflet</LoginButton>
+                    <LoginButton>{postLeaflet}</LoginButton>
                 </div>
             </form>
         </div>
--- src/Pages/Leaflet/HouseTuitionLeaflet.jsx
+++ src/Pages/Leaflet/HouseTuitionLeaflet.jsx
@@ -6,7 +6,8 @@ import { useHeader } from '../../Contexts/HeaderProvider';
 
 const HouseTuition = () => {
 
-    const { darkMode } = useHeader();
+    const { darkMode, translations } = useHeader();
+    const { heading, aboutTutor, address, division, district, upazila, area, status, student, subject, qualification, institute, gender, maleFemale, tutorsDemand, description, describe, salary, negotiable, term, termLink, postLeaflet } = translations.leafletHouseTuition;
     const { register, handleSubmit, formState: { errors } } = useForm();
 
     const handleLeaflet = data => {
@@ -15,71 +16,71 @@ const HouseTuition = () => {
 
     return (
         <div className={`w-5/6 mx-auto p-2 sm:p-7 rounded ${darkMode ? 'dark' : 'bg-grayBody text-blue-Foot'}`}>
-            <h2 className='text-xl sm:text-2xl font-bold text-center text-blueFoot dark:text-grayBody mb-10'>This information will assist you in reaching more people and receiving faster responses.</h2>
+            <h2 className='text-xl sm:text-2xl font-bold text-center text-blueFoot dark:text-grayBody mb-10'>{heading}</h2>
             <form onSubmit={handleSubmit(handleLeaflet)}>
                 <div className='md:flex gap-6'>
                     <div className='sm:w-11/12 md:w-1/2 mx-auto bg-base-100 shadow-md shadow-darkColor dark:shadow-grayBody p-5 rounded space-y-2 mb-6 md:mb-0'>
-                        <h2 className='text-center font-semibold text-lg underline'>About Tutor</h2>
+                        <h2 className='text-center font-semibold text-lg underline'>{aboutTutor}</h2>
                         <div>
-                            <label htmlFor="" className='font-bold'>Address :</label>
+                            <label htmlFor="" className='font-bold'>{address} :</label>
                             <div className='grid grid-cols-1 md:grid-cols-2 gap-5 ms-16'>
                                 <select name="" id="" className='p-2 focus:outline-none'>
-                                    <option>Division</option>
+                                    <option>{division}</option>
                                 </select>
                                 <select name="" id="" className='p-2 focus:outline-none'>
-                                    <option>District</option>
+                                    <option>{district}</option>
                                 </select>
                                 <select name="" id="" className='p-2 focus:outline-none'>
-                                    <option>Upazila</option>
+                                    <option>{upazila}</option>
                                 </select>
-                                <input type="text" placeholder='Your current location' className='p-2 focus:outline-none'></input>
+                                <input type="text" placeholder={area} className='p-2 focus:outline-none'></input>
                             </div>
                         </div>
                         <div>
-                            <label htmlFor="" className='font-bold'>Status :</label>
+                            <label htmlFor="" className='font-bold'>{status} :</label>
                             <div className='grid grid-cols-1 md:grid-cols-2 gap-5 ms-16'>
                                 <select name="" id="" className='p-2 focus:outline-none'>
-                                    <option>Student/Teacher/Other</option>
+                                    <option>{student}</option>
                                 </select>
                                 <select name="" id="" className='p-2 focus:outline-none'>
-                                    <option>Your Subject</option>
+                                    <option>{subject}</option>
                                 </select>
                                 <select name="" id="" className='p-2 focus:outline-none'>
-                                    <option>Qualification</option>
+                                    <option>{qualification}</option>
                                 </select>
-                                <input type="text" placeholder='Institute Name, where you are learning/working' className='p-2 focus:outline-none'></input>
+                                <input type="text" placeholder={institute} className='p-2 focus:outline-none'></input>
                             </div>
                         </div>
                         <div>
-                            <label htmlFor="" className='font-bold'>Gender :</label>
+                            <label htmlFor="" className='font-bold'>{gender} :</label>
                             <select name="" id="" className='p-2 block ms-16 focus:outline-none'>
-                                <option>Male/Female</option>
+                                <option>{maleFemale}</option>
                             </select>
                         </div>
                     </div>
 
                     <div className='sm:w-11/12 md:w-1/2 mx-auto bg-base-100 shadow-md shadow-darkColor dark:shadow-grayBody p-5 rounded space-y-2'>
-                        <h2 className='text-center font-semibold text-lg underline'>Tutor's Demand</h2>
+                        <h2 className='text-center font-semibold text-lg underline'>{tutorsDemand}</h2>
                         <div>
-                            <label htmlFor="" className='font-bold'>Expected Salary(tk/-) :</label>
+                            <label htmlFor="" className='font-bold'>{description} :</label>
+                            <textarea name="" id="" cols="30" rows="10" className='p-2 block sm:ms-16 focus:outline-none' placeholder={describe}></textarea>
+                        </div>
+                        <div>
+                            <label htmlFor="" className='font-bold'>{salary} :</label>
                             <input type="number" name="" id="" className='p-2 block sm:ms-16 focus:outline-none'></input>
                         </div>
                         <div className="flex items-center my-5">
                             <input id="default-checkbox" type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-white border-gray-300 rounded" />
-                            <label htmlFor="default-checkbox" className="ml-2 text-sm font-medium">Negotiable</label>
-                        </div>
-                        <div>
-                            <label htmlFor="" className='font-bold'>Description :</label>
-                            <textarea name="" id="" cols="30" rows="10" className='p-2 block sm:ms-16 focus:outline-none' placeholder="Please write here the class and subjects you want to teach. For example, Class: 6 to 12, Subjects: Mathematics, ICT."></textarea>
+                            <label htmlFor="default-checkbox" className="ml-2 text-sm font-medium">{negotiable}</label>
                         </div>
                     </div>
                 </div>
                 <div className="flex items-center my-5">
                     <input id="default-checkbox" type="checkbox" value="" className="w-4 h-4 text-blueFoot bg-gray-100 border-gray-300 rounded" />
-                    <label htmlFor="default-checkbox" className="ml-2 text-sm font-medium">I have read and accept the <Link to='/terms' className='text-yellowBtn'>terms and conditions</Link></label>
+                    <label htmlFor="default-checkbox" className="ml-2 text-sm font-medium">{term} <Link to='/terms' className='text-yellowBtn'>{termLink}</Link></label>
                 </div>
                 <div className='w-52 mx-auto'>
-                    <LoginButton>Post Leaflet</LoginButton>
+                    <LoginButton>{postLeaflet}</LoginButton>
                 </div>
             </form>
         </div>
